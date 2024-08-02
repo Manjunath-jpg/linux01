d1e2cd52151bbd9ecac672af4d5791e8016ab9ca
kk <kk@gmail.com>
2024-08-02 12:36:42 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,5 @@
+print("This is first python")
+#for finding how many hours worked
+hours = float(input("enter in HOURS= "))
+rate = float(input("enter in RATE= "))
+print("TOTAL = ",hours * rate, "$_DOLLERS")
